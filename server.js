b0c7b5bf0eb7882ee0c60ad6928bbf3743e80a44
hoverzhu <34266699+hoverzhu@users.noreply.github.com>
2021-01-22 11:26:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello world!');
+  response.end('Hello World v1!');
 };
 var www = http.createServer(handleRequest);
 www.listen(80);
